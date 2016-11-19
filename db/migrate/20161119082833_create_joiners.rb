984c5b0fd809e9555b629b3754aa6f75d8b628c2
Nguyen Quang Huy <huy@nhom635.com>
2016-11-19 16:31:51 +0700

    Database thieu test, sign up login logout day du

---
+++ db/migrate/20161119082833_create_joiners.rb
@@ -0,0 +1,11 @@
+class CreateJoiners < ActiveRecord::Migration[5.0]
+  def change
+    create_table :joiners do |t|
+      t.references :user, foreign_key: true
+      t.references :work, foreign_key: true
+      t.date :joined_date
+
+      t.timestamps
+    end
+  end
+end
