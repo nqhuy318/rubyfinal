812f9a7c014aac8014481ac93615ead83b7231eb
KIEN <hama2510@gmail.com>
2016-11-21 20:21:21 +0700

    sửa giao diện search work

--- app/controllers/search_works_controller.rb
+++ app/controllers/search_works_controller.rb
@@ -1,12 +1,17 @@
 class SearchWorksController < ApplicationController
   def index
     @user = current_user
-    if @user[:role_id] == 1
-      @works = Work.joins(:work_categories).joins(:categories)
-      .joins(:user).where(status: 0, :work_categories=>{category_id:1}).distinct
+    if @user.nil?
+      flash[:danger] = "Please login"
+      redirect_to login_path
     else
-      flash[:danger] = "You don't have permission"
-      redirect_to @user
+      if @user[:role_id] == 1
+        @works = Work.joins(:work_categories).joins(:categories)
+        .joins(:user).where(status: 0, :work_categories=>{category_id:1}).distinct
+      else
+        flash[:danger] = "You don't have permission"
+        redirect_to @user
+      end
     end
   end
 end
