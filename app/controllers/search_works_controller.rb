2c5c4635eead1d322c02ef98c02fd4468c9d7a55
KIEN <hama2510@gmail.com>
2016-11-22 23:11:24 +0700

    search work
    search work với tính năng hiện và sắp xếp matching skill vs not matching
    skill

--- app/controllers/search_works_controller.rb
+++ app/controllers/search_works_controller.rb
@@ -6,8 +6,41 @@ class SearchWorksController < ApplicationController
       redirect_to login_path
     else
       if @user[:role_id] == 1
-        @works = Work.joins(:work_categories).joins(:categories)
-        .joins(:user).where(status: 0, :work_categories=>{category_id:1}).distinct
+        @works = Work.find_by_sql("with tbl_user AS(
+select users.id AS user_id, categories.id AS category_id
+  from users , categories,
+ freelancer_categories AS fc where users.id = fc.user_id AND 
+ categories.id = fc.category_id
+), tbl_work AS(
+select works.id AS work_id, categories.id AS category_id
+  from works , categories,
+ work_categories AS wc where works.id = wc.work_id AND 
+ categories.id = wc.category_id
+), tbl_wu AS(
+select tw.work_id, tw.category_id, tu.user_id
+ from tbl_work AS tw
+ LEFT JOIN  tbl_user AS tu 
+ON tw.category_id = tu.category_id 
+), tbl_match AS(
+select work_id, user_id, count() AS match_num from tbl_wu 
+ where user_id IS NOT NULL
+ GROUP BY work_id, user_id
+ ), tbl_not_match AS(
+select work_id, user_id, count() AS not_match_num from tbl_wu 
+ where user_id IS NULL
+ GROUP BY work_id, user_id
+ )
+ SELECT tm.work_id, 
+ CASE WHEN tnm.not_match_num IS NULL 
+ THEN 0 ELSE tnm.not_match_num END AS not_match_num, 
+ tm.match_num , works.name, works.price, users.fullname
+ from tbl_match AS tm LEFT JOIN tbl_not_match AS tnm
+ ON tnm.work_id = tm.work_id
+ JOIN works ON tm.work_id = works.id
+ JOIN users ON works.user_id = users.id
+ WHERE works.status = 0
+ ORDER BY not_match_num ASC, match_num DESC
+          ")
       else
         flash[:danger] = "You don't have permission"
         redirect_to @user
