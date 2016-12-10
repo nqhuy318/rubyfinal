970aeaeeb919f8b8cf58ba11700c10184c191a03
KIEN <hama2510@gmail.com>
2016-12-10 20:17:44 +0700

    Revert "sửa take test"
    This reverts commit 5416fca4440ef2d7c4ab80b14d74f23721c8d15e.

--- app/helpers/take_test_helper.rb
+++ app/helpers/take_test_helper.rb
@@ -6,24 +6,22 @@ module TakeTestHelper
   def current_test
     @current_test = session[:questions]
   end
+  #  
+  #  def current_question(index)
+  #    @current_test ||= Question.find_by(id: session[:questions][index][:id])
+  #  end
   
-  def is_loaded?
-    unless session[:questions].nil?
+  def get_true_answer(question)
+    @true_answer = Answer.joins(:question).where(tf:1, questions:{id: question})
+    logger.debug @true_answer.to_yaml
+  end
+  
+  def check_answer(answer)
+    @true_answer = Answer.where(tf:1, id: answer)
+    if @true_answer.empty?
+      return false
+    else
       return true
-    end 
-    return false
-  end
-  
-  def destroy_test
-    session.delete(:questions)
-    @current_test = nil
-  end
-  
-  def get_time
-    @count_down = session[:time]
-  end
-  
-  def set_count_down_time(time)
-    session[:time] = time
+    end
   end
 end
